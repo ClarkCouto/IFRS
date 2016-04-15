e0ab1b971171a4ca37329a8bed2024a426be642d
thayseonofrio <thayseonofrio@hotmail.com>
2016-04-15 19:24:27 -0300

    Exercicio Carros atualizado

--- Thayse/Páginas Web/PHP08-04/carros.php
+++ Thayse/Páginas Web/PHP08-04/carros.php
@@ -7,15 +7,46 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        
+        <div>
+        <form method="POST">
+            <p> <label for="carro1">Insira o nome do carro 01:</label> 
+                <input type="text" name="carro1"> </p>
+            <br>
+            <p> <label for="carro2">Insira o nome do carro 02:</label> 
+                <input type="text" name="carro2"> </p>
+            <br>
+            <p> <label for="carro3">Insira o nome do carro 03:</label> 
+                <input type="text" name="carro3"> </p>
+            <br>
+            <p> <label for="carro4">Insira o nome do carro 04:</label> 
+                <input type="text" name="carro4"> </p>
+            <br>
+            <p> <label for="carro5">Insira o nome do carro 05:</label> 
+                <input type="text" name="carro5"> </p>
+            <br>
+            <input type="submit" value="Enviar" name="submit">
+            <input type="button" value="Voltar" onclick="history.back()">
+        </form>
+        </div>
+        <br>
+        <br>
+        <br>
         <?php
-        $carros = array("uno", "gol", "strada", "duster", "fox");
         
-        echo '<div>';
-        foreach ($carros as $value) {
-            echo $value."<br>";
+        if (isset($_POST["submit"])) { 
+            $carros = [];
+            for ($i = 1; $i < 6; $i++) {
+                 $carros[$i-1] = $_POST["carro$i"];
+            }
+
+
+            echo '<div>';
+            foreach ($carros as $value) {
+                echo $value."<br>";
+            }
+            echo '</div>';
         }
-        echo '</div>';
+        
         ?>
     </body>
 </html>
