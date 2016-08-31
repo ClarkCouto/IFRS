8f164f5d6acc54133606c98a765f623fa7b1e1d2
Matheus Ligabue <matheus.s.ligabue@gmail.com>
2016-08-31 11:32:55 -0300

    [Matheus] Mockup da tarefa de tipografia

--- 2016-02/Matheus/Estrutura de Dados 2/ArvoresBinarias/main.c
+++ 2016-02/Matheus/Estrutura de Dados 2/ArvoresBinarias/main.c
@@ -13,49 +13,8 @@ typedef struct nodo {
     struct nodo *pai, *esq, *dir;    
 } nodo;
 
-nodo *cria_nodo (int v, nodo *raiz) {
-    // Cria novo nodo e aloca seu espaço requerido em memória
-    nodo *novo;
-    novo = (nodo *)malloc(sizeof(nodo));
-    
-    // Aponta para o nodo pai e define os filhos como NULL
-    novo->pai = raiz;
-    novo->esq, novo->dir = NULL;
-    
-    // Retorna o nodo criado
-    return novo;
-}
 
-// Testa se o elemento é uma raiz ou nodo.;
-// Se for raiz: retorna 1. Se for nodo: retorna 0. Se não existir: retorna 9.
-int testa_raiz (nodo *nodo) {
-    int teste = 9; // Assume que não existe por padrão
-    if (nodo != NULL)
-        if (nodo->esq != NULL && nodo->dir != NULL)
-            teste = 1; // É raiz
-        else
-            teste = 0; // É nodo
-    return teste;
-}
-
-//nodo *insere (nodo *raiz, int v) {    ;
-//    
-//    // Cria novo nodo
-//    
-//    // Se nodo for raiz, navega para o próximo nodo
-//    if (testa_raiz == 1)
-//        if (raiz->valor > v)
-//            insere(raiz->esq, v);
-//        else
-//            insere(raiz->dir, v);
-//    // Se nodo for folha, insere uma nova folha
-//    else if (testa_raiz == 0)       
-//        
-//}
-
-int main(int argc, char** argv) {
-    
-    nodo *raiz;
+int main(int argc, char** argv) {   
     
     return (EXIT_SUCCESS);
 }
