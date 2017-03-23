8813d53796627ab5b72d4c66eeb45039554b8d0c
MATHEUS LIGABUE DA SILVA <MATHEUS LIGABUE DA SILVA>
2017-03-23 20:35:22 -0300

    ED-II: Finalizado algoritmo de inserção

--- 2017-01/Matheus/Estrutura de Dados/ArvoreBinaria/arvore_binaria_busca.h
+++ 2017-01/Matheus/Estrutura de Dados/ArvoreBinaria/arvore_binaria_busca.h
@@ -17,53 +17,62 @@ enum ordem_percurso {pre_ordem, em_ordem, pos_ordem};
 
 // Funções
 
-folha * insere_folha(folha *nova, folha *raiz){
-       
-    nova->raiz = raiz;
-    
-    if (raiz == NULL){
-        printf("ERRO: Impossível inserir pois a raiz não foi criada.\n"
-               "Utilize a função cria_folha() para criar a raiz da árvore.");
-    } else {
-        if (nova->valor < raiz->valor){
-            raiz->esquerda = nova;
-        } else if (nova->valor > raiz->valor){
-            raiz->direita = nova;
-        } else {
-           printf("ERRO: Este valor já existe na lista.\n"
-                  "Não são permitidos valores duplicados.") ;
-        }
-    }
-    
-    return raiz;
-}
-
-void imprime_arvore(folha *folha) {      
-    printf("R: ");
-    printf("%d\n", folha->valor);    
-    
-    if (folha->esquerda != NULL) {
-        imprime_arvore(folha->esquerda);        
-    }
-    
-    if (folha->direita != NULL) {
-        imprime_arvore(folha->direita);
-    }
-}
-
-folha * cria_folha(int valor){
+folha * bt_new(int valor, folha *pai){
     
     folha *nova;
     nova = (folha *)malloc(sizeof(folha));        
     
     nova->valor = valor;    
-    nova->raiz = NULL;
+    nova->raiz = pai;
     nova->esquerda = NULL;
     nova->direita = NULL;
 
     return nova;
 }
 
+folha * bt_add(int valor, folha *raiz){           
+    if (raiz == NULL){
+        printf("ERRO: Impossível inserir pois a raiz não foi criada.\n"
+               "Utilize a função cria_folha() para criar a raiz da árvore.\n\n");
+        return raiz;
+    }
+    
+    if (valor < raiz->valor)
+        if (raiz->esquerda == NULL)
+            raiz->esquerda = bt_new(valor, raiz);
+        else
+            raiz->esquerda = bt_add(valor, raiz->esquerda);
+    else if (valor > raiz->valor)     
+        if (raiz->direita == NULL)
+            raiz->direita = bt_new(valor, raiz);
+        else
+            raiz->direita = bt_add(valor, raiz->direita);
+    else {
+        printf("ERRO: Impossível inserir pois a raiz não foi criada.\n"
+               "Utilize a função cria_folha() para criar a raiz da árvore.\n\n");
+        return raiz;
+    }       
+    
+    return raiz;
+}
+
+void imprime_arvore(folha *folha) {
+    if (folha->raiz == NULL) {
+      printf("R >");
+    }
+
+    printf("%d\n", folha->valor);    
+    
+    if (folha->esquerda != NULL) {
+        printf("E >");
+        imprime_arvore(folha->esquerda);        
+    }
+    
+    if (folha->direita != NULL) {
+        printf("D >");
+        imprime_arvore(folha->direita);
+    }
+}
 
 
 #endif /* ARVORE_BINARIA_H */
--- 2017-01/Matheus/Estrutura de Dados/ArvoreBinaria/main.c
+++ 2017-01/Matheus/Estrutura de Dados/ArvoreBinaria/main.c
@@ -2,9 +2,14 @@
 
 int main(int argc, char** argv) {
     
-    folha *arvore = cria_folha(2);
-    arvore = insere(cria_folha(1), arvore);
-
+    folha *arvore = NULL;
+    
+    arvore = bt_new(2, arvore);
+    arvore = bt_add(1, arvore);
+    arvore = bt_add(3, arvore);
+    arvore = bt_add(4, arvore);
+    arvore = bt_add(6, arvore);
+    arvore = bt_add(5, arvore);
     imprime_arvore(arvore);
     
     return (EXIT_SUCCESS);
